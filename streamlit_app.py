1f6b7b5816eeba3cd82a3652c075a8d94665050b
Siddhant Sapte <48004110+siddsapte12@users.noreply.github.com>
2022-07-01 17:31:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
   streamlit.error()
 
 ##
-streamlit.header("View Our Fruit List - Add Your Favorites!:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list") 
